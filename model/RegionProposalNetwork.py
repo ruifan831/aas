bf057b0d1f40e475108c34575b948ba15cfd1fa8
ruifanxu_Linux <ruifanxu@usc.edu>
2021-04-11 22:06:58 -0700

    update

--- model/RegionProposalNetwork.py
+++ model/RegionProposalNetwork.py
@@ -1,68 +1,40 @@
 from torch import nn
 import torch.nn.functional as F
-from utils.anchor import anchor_generator
-
+from utils.anchor import AnchorGenerator
 
 
 class RPNHead(nn.Module):
-    def __init__(self,in_channels,num_anchors):
-        super(RPNHead,self).__init__()
-        self.conv = nn.Conv2d(in_channels,in_channels,kernel_size = 3, stride=1, padding=1)
-        self.cls_pred = nn.Conv2d(in_channels,num_anchors*2,kernel_size = 1,stride =1)
-        self.offset_pred = nn.Conv2d(in_channels, num_anchors*4, kernel_size=1, stride=1)
-    
-    def forward(self,x):
+    def __init__(self, in_channels, num_anchors):
+        super(RPNHead, self).__init__()
+        self.conv = nn.Conv2d(in_channels, in_channels,
+                              kernel_size=3, stride=1, padding=1)
+        self.cls_pred = nn.Conv2d(
+            in_channels, num_anchors*2, kernel_size=1, stride=1)
+        self.offset_pred = nn.Conv2d(
+            in_channels, num_anchors*4, kernel_size=1, stride=1)
+
+    def forward(self, x):
         h = F.relu(self.conv(x))
         cls_pred = self.cls_pred(h)
         offset_pred = self.offset_pred(h)
-        return cls_pred,offset_pred
+        return cls_pred, offset_pred
+
 
 class RegionProposalNetwork(nn.Module):
-    def __init__(self,in_channels=512,mid_channels=512,ratios=[0.5,1,2],anchor_scales=[8,16,32],feat_stride=16,proposal_creator_params=dict()):
-        super(RegionProposalNetwork,self).__init__()
-        self.anchor_base = anchor_generator(ratios=ratios,anchor_scales=anchor_scales)
-        self.feat_stride = feat_stride
-        n_anchor=self.anchor_base.shape[0]
-        self.conv1 = nn.Conv2d(in_channels,mid_channels,3,1,1)
-        self.score = nn.Conv2d(mid_channels,n_anchor*2,1,1,0)
-        self.loc = nn.Conv2d(mid_channels,n_anchor*4,1,1,0)
-    
+    def __init__(self, rpn_anchor_generator, head,fg_iou_thresh, bg_iou_thresh, batch_size_per_images, positive_fraction,pre_nms_top_n, post_nms_top_n, nms_thresh, score_thresh=0.0):
+        super(RegionProposalNetwork, self).__init__()
+        self.anchor_generator = rpn_anchor_generator
+        self.rpn_head = head
 
-    def forward(self,x,img_size,scale=1):
-        n,_,h,w = x.shape
-        anchors = generate_anchors(self.anchor_base,self.feat_stride,h,w)
-        n_anchor = anchors.shape[0]//(h*w)
-        h = F.relu(self.conv1(x))
-
-        # rpn_score shape: (N, len(ratios)*len(anchor_scales)*4,H,W)
-        rpn_offset = self.loc(h)
-        rpn_offset = rpn_offset.permute(0,2,3,1).contiguous().view(n,-1,4)
-
-        # rpn_score shape: (N, len(ratios)*len(anchor_scales)*2,H,W)
-        rpn_score = self.score(h)
+    def forward(self, x, img_shape):
+        n,_,h,w=x.shape
+        anchors = self.anchor_generator(img_shape, x)
+        n_anchor = anchors.shape[1]
+        rpn_score,rpn_offset = self.rpn_head(x)
         rpn_score = rpn_score.permute(0,2,3,1).contiguous()
-        rpn_softmax_score = F.softmax(rpn_score.view(n,h,w,n_anchor,2),dim=4)
-        rpn_fg_scores = rpn_softmax_score[:,:,:,:,1].contiguous().view(n,-1)
+        rpn_softmax_scores = F.softmax(rpn_score.view(n,h,w,n_anchor,2),dim=4)
+        rpn_fg_scores=rpn_softmax_scores[:,:,:,:,1].contiguous()
+        rpn_score=rpn_fg_scores.view(n,-1)
         rpn_score = rpn_score.view(n,-1,2)
-
-        rois = list()
-        roi_indices = list()
-
         
-
-
-
-def generate_anchors(anchor_base,feat_stride,height,width):
-    import numpy as np
-    shift_y = np.arange(0,height*feat_stride,feat_stride)
-    shift_x = np.arange(0,width*feat_stride,feat_stride)
-    shift_x,shift_y = np.meshgrid(shift_x,shift_y)
-    shift = np.stack((shift_y.ravel(),shift_x.ravel(),shift_y.ravel(),shift_x.ravel()),axis=1)
-    A = anchor_base.shape[0]
-    # K is the number of coordinate that will have anchor on it.
-    K = shift.shape
-
-    anchor = anchor_base.reshape((1,A,4))+shift.reshape((K,1,4))
-    anchor = anchor.reshape((K*A,4)).astype(np.float32)
-    return anchor
 
--- model/utils/anchor.py
+++ model/utils/anchor.py
@@ -1,45 +1,64 @@
 import numpy as np
 import torch as t
+from torch import nn
 
 class AnchorGenerator(nn.Module):
-    def __init__(self,ratios=[0.5,1,2],anchor_scales=[8,16,32],base_size=16):
+    def __init__(self,ratios=[[0.5,1,2]],anchor_scales=[[8,16,32]],base_size=16):
         super().__init__()
         self.ratios = ratios
         self.anchor_scales= anchor_scales
         self.base_size= base_size
+        self.cell_anchors =None
     
     def _ratio_enum(self,ratios):
-        h = self.base_size*t.sqrt(self.ratios)
-        w = self.base_size*t.sqrt(1/t.as_tensor(self.ratios))
+        h = self.base_size*t.sqrt(ratios)
+        w = self.base_size*t.sqrt(1/ratios)
         return h,w
+    def num_anchors_per_location(self):
+        return [len(s) * len(a) for s, a in zip(self.sizes, self.anchor_scales)]
     
-    def generate_anchors(self,dtype=torch.float32,device="cpu"):
-        h,w = self._ratio_enum()
-
-
-
-
-def anchor_generator(ratios=[0.5,1,2],anchor_scales=[8,16,32],base_size=16):
-    h,w = _ratio_enum(base_size, ratios)
-    x_ctr = (base_size-1)/2
-    y_ctr = (base_size-1)/2
-    anchor_base = np.zeros((len(ratios)*len(anchor_scales),4),dtype = np.float32)
-    for i,scale in enumerate(anchor_scales):
-        for j in range(3):
-            index = i*len(anchor_scales) +j
-            anchor_base[index,0] = y_ctr - (h[j]*scale-1)/2
-            anchor_base[index,1] = x_ctr - (w[j]*scale-1)/2
-            anchor_base[index,2] = y_ctr + (h[j]*scale-1)/2
-            anchor_base[index,3] = x_ctr + (w[j]*scale-1)/2
-    return anchor_base
-             
-
-
-
-def _ratio_enum(base_size,ratios):
-    h = base_size*np.sqrt(ratios)
-    w = base_size*np.sqrt(1/np.array(ratios))
-    return h,w
-
+    def generate_anchors(self,ratios,scales,dtype=t.float32,device="cpu"):
+        ratios = t.as_tensor(ratios, dtype=dtype, device=device)
+        anchor_scales = t.as_tensor(scales, dtype=dtype, device=device)
+        h,w = self._ratio_enum(ratios)
+        hs = (anchor_scales.view(-1,1)*h.view(1,-1)).view(-1)
+        ws = (anchor_scales.view(-1,1)*w.view(1,-1)).view(-1)
+        base_anchor = t.stack((-hs,-ws,hs,ws),dim=1)/2
+        return base_anchor
+    
+    def set_cell_anchors(self,dtype,device):
+        if self.cell_anchors is not None:
+            cell_anchors = self.cell_anchors
+            assert cell_anchors is not None
+            if cell_anchors[0].device == device:
+                return
+        cell_anchors=[self.generate_anchors(ratios,scales,dtype,device) for ratios,scales in zip(self.ratios,self,anchor_scales)]
+        self.cell_anchors = cell_anchors
+    def forward(self,imgs,feature_maps):
+        # feature_map: (N,C,H,W)
+        grid_sizes=[feature_map.shape[-2:] for feature_map in feature_maps] # (N,2)
+        img_size = imgs.tensors.shape[-2:] # (N，2）
+        dtype,device = feature_maps[0].dtype,feature_maps[0].device
+        strides= [[t.tensor(img_size[0]//g[0],dtype=t.int64,device=device),
+                  t.tensor(img_size[1]//g[1],dtype=t.int64,device=device)] for g in grid_sizes]
+        self.set_cell_anchors(dtype,device)
+        anchors_over_feature_maps = self.grid_anchors(grid_sizes,strides)
+        return anchors_over_feature_maps
+    
+    def grid_anchors(self,grid_sizes,strides):
+        anchors = []
+        for size,stride in zip(grid_sizes,strides):
+            grid_height,grid_width = size
+            stride_height,stride_width = stride
+            device = self.cell_anchors.device
+            shifts_y = t.arange(0,grid_height,dtype=t.float32,device=device)*stride_height
+            shifts_x = t.arange(0,grid_width,dtype=t.float32,device=device)*stride_width
+            shift_x,shift_y = t.meshgrid(shifts_x,shifts_y)
+            shift_y = shift_y.reshape(-1)
+            shift_x = shift_x.reshape(-1)
+            shifts = t.stack((shift_y,shift_x,shift_y,shift_y),dim=1)
+            anchors.append((shifts.view(-1,1,4)+self.cell_anchors.view(1,-1,4)).reshape(-1,4))
+        # anchors: (N,number_of_grid *9,4)
+        return t.as_tensor(anchors)
 
 
