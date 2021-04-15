7fc868baeabedd21f482bb572649e08215e98a11
RUIFANXU <ruifan831@gmail.com>
2021-04-14 22:26:48 -0700

    update

---
+++ model/Backbone.py
@@ -0,0 +1,31 @@
+from torchvision.models import vgg16
+from torch import nn
+
+class Backbone(nn.Module):
+
+    def __init__(self,backbone):
+        super().__init__()
+        self.backbone = backbone
+    
+    def forward(self,x):
+        x = self.backbone(x)
+        return x
+
+
+def vggbackbone(pretrained = True):
+    backbone = vgg16(pretrained)
+    features = list(backbone.features)[:30]
+    classifier = backbone.classifier
+
+    classifier = list(classifier)
+    del classifier[6]
+    classifier = nn.Sequential(*classifier)
+
+    # freeze top4 conv
+    for layer in features[:10]:
+        for p in layer.parameters():
+            p.requires_grad = False
+    backbone = Backbone(nn.Sequential(*features))
+    return backbone
+
+
